53fb0ada6ede1719117b0a096fa9172ee0d8be4f
LunaTheFox20 <80362356+LunaTheFox20@users.noreply.github.com>
2023-12-21 18:54:38 +0100

    Update main.rs
    Improved Error handling

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,72 @@
-macro_rules! log {
-    ($msg:expr) => {
-        println!("[LOG] {}", $msg);
-    };
-}
-
-macro_rules! error {
-    ($msg:expr) => {
-        println!("[ERROR] {}", $msg);
-    };
-}
-
 use argon2::{
     password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
     Argon2,
 };
 use rand_core::OsRng;
+use std::error::Error;
 use std::io::{self, Write};
 
-fn main() {
+use std::{error, fmt};
+
+#[derive(Debug)]
+struct ArgonError(argon2::password_hash::Error);
+
+impl fmt::Display for ArgonError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl error::Error for ArgonError {}
+
+impl From<argon2::password_hash::Error> for ArgonError {
+    fn from(err: argon2::password_hash::Error) -> ArgonError {
+        ArgonError(err)
+    }
+}
+
+macro_rules! log {
+    ($msg:expr) => {
+        eprintln!("[LOG] {}", $msg);
+    };
+}
+
+macro_rules! error {
+    ($msg:expr) => {
+        eprintln!("[ERROR] {}", $msg);
+    };
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let mut password = String::new();
     print!("Please enter your password: ");
-    io::stdout().flush().unwrap(); // flush it to the screen
+    io::stdout().flush()?; // flush it to the screen
 
-    io::stdin().read_line(&mut password).unwrap();
+    io::stdin().read_line(&mut password)?;
     let password = password.trim().as_bytes();
     if password.is_empty() {
         error!("You have to provide a password!");
-        return;
+        return Ok(());
     }
 
     let salt = SaltString::generate(&mut OsRng);
 
     // Argon2 with default params (Argon2id v19)
-    let argon2 = Argon2::new(
-        argon2::Algorithm::Argon2i,
-        argon2::Version::V0x13,
-        argon2::Params::new(15000, 2, 1, None).unwrap(),
-    );
+    let argon2 = Argon2::default();
 
     // Hash password to PHC string ($argon2id$v=19$...)
-    let password_hash = argon2.hash_password(password, salt.as_salt()).unwrap();
+    let password_hash = argon2
+        .hash_password(password, salt.as_salt())
+        .map_err(ArgonError)?;
     log!("Password hashed successfully");
-    println!("[LOG] Generated hash: {}", password_hash);
+    eprintln!("[LOG] Generated hash: {}", password_hash);
 
     // Verify password against PHC string.
     let binding = password_hash.to_string();
-    let parsed_hash = match PasswordHash::new(&binding) {
-        Ok(hash) => {
-            log!("Hash parsed successfully");
-            hash
-        }
-        Err(err) => {
-            eprintln!("Error parsing hash: {:?}", err);
-            return;
-        }
-    };
+    let parsed_hash = PasswordHash::new(&binding).map_err(ArgonError)?;
 
     assert!(argon2.verify_password(password, &parsed_hash).is_ok());
     log!("Password verified successfully");
+
+    Ok(())
 }
