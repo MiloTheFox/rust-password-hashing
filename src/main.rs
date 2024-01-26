f2529c028f58a6cef7e2a381f310ff38c2cbed6f
LunaTheFox20 <80362356+LunaTheFox20@users.noreply.github.com>
2024-01-27 00:32:48 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,45 @@
-// Importing necessary modules and functions
 use argon2::{
     password_hash::{PasswordHasher, PasswordVerifier, SaltString},
     Algorithm, Argon2, ParamsBuilder,
 };
-use rand_core::OsRng; // For generating random numbers
-use zeroize::Zeroize; // For securely erasing sensitive data
-
-use rpassword::prompt_password; // Used to hide the password input
-
-use std::error::Error;
-use std::fmt;
-
-// Import colored module for colored output
 use colored::Colorize;
+use rand_core::OsRng;
+use rpassword::prompt_password;
+use zeroize::Zeroize;
 
-
-// Define a new error type for Argon2 errors
-#[derive(Debug)]
-pub struct ArgonError(pub argon2::password_hash::Error);
-
-// Define a new error type for our application
-#[derive(Debug)]
-pub enum MyError {
-    Argon(ArgonError),
-    VerificationFailed,
-}
-
-impl fmt::Display for ArgonError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.0)
+fn main() {
+    let mut password = prompt_password("Please enter your password: ")
+        .unwrap()
+        .trim()
+        .to_string();
+    if password.is_empty() {
+        eprintln!("{}", "[ERROR] You have to provide a password!".bold().red());
+        return;
     }
-}
-
-impl fmt::Display for MyError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            MyError::Argon(e) => write!(f, "Argon2 error: {}", e),
-            MyError::VerificationFailed => write!(f, "Password verification failed"),
-        }
-    }
-}
-
-impl Error for MyError {}
-
-// Define a macro for logging messages
-macro_rules! log {
-    ($msg:expr) => {
-        eprintln!("{}", format!("[LOG] {}", $msg).green());
-    };
-}
-
-// Define a macro for logging error messages
-macro_rules! error {
-    ($msg:expr) => {
-        eprintln!("{}", format!("[ERROR] {}", $msg).bold().red());
-    };
-}
-
-// Main function
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut password = prompt_password("Please enter your password: ")?;
-    let password_trimmed = password.trim();
-    if password_trimmed.is_empty() {
-        error!("You have to provide a password!");
-        return Ok(());
-    }
-
-    let password_bytes = password_trimmed.as_bytes();
 
     let salt = SaltString::generate(&mut OsRng);
-
-    let params = ParamsBuilder::new()
-        .m_cost(19) // reduced memory cost
-        .t_cost(2) // reduced time cost
-        .p_cost(1) // reduced parallelism cost
-        .output_len(32) // reduced output length
+    let params = ParamsBuilder::default()
+        .m_cost(19)
+        .t_cost(2)
+        .p_cost(1)
+        .output_len(32)
         .build()
-        .expect("Failed to build Argon2 parameters");
-
+        .unwrap();
     let argon2 = Argon2::new(Algorithm::Argon2id, argon2::Version::V0x13, params);
-
     let password_hash = argon2
-        .hash_password(password_bytes, salt.as_salt())
-        .expect("Failed to hash password");
-    log!("Password hashed successfully");
+        .hash_password(password.as_bytes(), salt.as_salt())
+        .unwrap();
+
+    eprintln!("{}", "[LOG] Password hashed successfully".green());
     println!("{} {}", "[LOG] Generated hash: ".yellow(), password_hash);
 
-    // Verify the password
     if argon2
-        .verify_password(password_bytes, &password_hash)
+        .verify_password(password.as_bytes(), &password_hash)
         .is_ok()
     {
-        log!("Password verified successfully");
         password.zeroize();
-        Ok(())
+        eprintln!("{}", "[LOG] Password verified successfully".green());
     } else {
-        error!("Password verification failed");
-        Err(Box::new(MyError::VerificationFailed))
+        eprintln!("{}", "[ERROR] Password verification failed".bold().red());
     }
 }
