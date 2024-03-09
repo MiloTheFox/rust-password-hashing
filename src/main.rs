3a24ecdd9d3dcd3b3c702dcc37c95714cc8dfb8d
LunaTheFox20 <80362356+LunaTheFox20@users.noreply.github.com>
2024-03-09 18:59:59 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use argon2::{password_hash::SaltString, Algorithm, Argon2, Params, PasswordHasher, Version};
 use colored::Colorize;
-use rand::rngs::StdRng;
-use rand::SeedableRng;
-use rand::{distributions::Alphanumeric, Rng};
+use passwords::{analyzer, scorer, PasswordGenerator};
 use rand_core::OsRng;
+use rayon::prelude::*;
 use std::fmt;
 use std::sync::Arc;
 use thiserror::Error;
-use tokio::task;
 
 #[derive(Debug)]
 pub struct ArgonError(argon2::password_hash::Error);
@@ -34,10 +32,8 @@ const TIME_COST: u32 = 8;
 const PARALLELISM: u32 = 16;
 const OUTPUT_LEN: usize = 64;
 
-#[tokio::main]
-async fn main() -> Result<(), MyError> {
-    let passwords: Vec<String> =
-        futures::future::join_all((0..50).map(|_| generate_password(16))).await;
+fn main() -> Result<(), MyError> {
+    let passwords = generate_passwords_using_rayon(50, 16);
 
     // Define Argon2 parameters
     let params_result = Params::new(MEMORY_COST, TIME_COST, PARALLELISM, Some(OUTPUT_LEN));
@@ -53,9 +49,10 @@ async fn main() -> Result<(), MyError> {
 
     let argon2 = Arc::new(Argon2::new(Algorithm::Argon2id, Version::V0x13, params));
 
-    let tasks = passwords.into_iter().map(|password| {
-        let argon2 = Arc::clone(&argon2);
-        task::spawn(async move {
+    let results: Vec<_> = passwords
+        .into_par_iter()
+        .map(|(password, _)| {
+            let argon2 = Arc::clone(&argon2);
             let rng = OsRng;
             let salt = SaltString::generate(*&rng);
             // Hash the password and handle any errors
@@ -63,46 +60,66 @@ async fn main() -> Result<(), MyError> {
                 .hash_password(password.as_bytes(), &salt)
                 .map(|hash| hash.to_string())
         })
-    });
+        .collect();
 
-    let results: Result<Vec<_>, _> = futures::future::try_join_all(tasks).await;
-
-    match results {
-        Ok(hashes) => {
-            for hash in hashes {
-                match hash {
-                    Ok(hash) => println!("Hashed password: {}", hash),
-                    Err(e) => println!("Failed to hash password: {}", e),
-                }
-            }
-            // Log success message
-            println!("{}", "[LOG] Passwords hashed successfully".green());
-        }
-        Err(e) => {
-            println!("Failed to hash password: {}", e);
+    for result in results {
+        match result {
+            Ok(hash) => println!("Hashed password: {}", hash),
+            Err(e) => println!("Failed to hash password: {}", e),
         }
     }
+    // Log success message
+    println!("{}", "[LOG] Passwords hashed successfully".green());
 
     Ok(())
 }
 
-async fn generate_password(length: u8) -> String {
-    let rng = StdRng::from_entropy(); // create a new StdRng
-    rng.sample_iter(&Alphanumeric)
-        .take(length as usize)
-        .map(char::from)
-        .collect::<String>()
+#[inline]
+fn generate_password(pg: &PasswordGenerator) -> (String, f64) {
+    let password = pg.generate_one().expect("Failed to generate password");
+    let analyzed: analyzer::AnalyzedPassword = analyzer::analyze(&password);
+    let score = scorer::score(&analyzed);
+    (password, score)
+}
+
+#[inline]
+fn generate_passwords_using_rayon(num_passwords: usize, length: usize) -> Vec<(String, f64)> {
+    let pg = PasswordGenerator {
+        length,
+        numbers: true,
+        lowercase_letters: true,
+        uppercase_letters: true,
+        symbols: true,
+        spaces: false,
+        exclude_similar_characters: true,
+        strict: true,
+    };
+
+    (0..num_passwords)
+        .into_par_iter()
+        .map(|_| generate_password(&pg))
+        .collect::<Vec<_>>() // Collect individual results
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[tokio::test]
-    async fn test_generate_password() {
-        let password_future = generate_password(16);
-        let password = password_future.await;
-        assert!(password.len() == 16 && password.chars().all(|c| c.is_alphanumeric()));
+    const PG: PasswordGenerator = PasswordGenerator {
+        length: 16, // replace `length` with the actual length
+        numbers: true,
+        lowercase_letters: true,
+        uppercase_letters: true,
+        symbols: true,
+        spaces: false,
+        exclude_similar_characters: true,
+        strict: true,
+    };
+
+    #[test]
+    fn test_generate_password() {
+        let (password, _score) = generate_password(&PG); // handle the tuple correctly
+        assert!(password.len() == 16 && password.chars().all(|c| c.is_ascii_graphic()));
     }
 
     #[test]
@@ -112,13 +129,13 @@ mod tests {
         assert_eq!(format!("{}", argon_error), "invalid password");
     }
 
-    #[tokio::test]
-    async fn test_hash_password() {
-        let password = generate_password(16);
+    #[test]
+    fn test_hash_password() {
+        let (password, _score) = generate_password(&PG); // handle the tuple correctly
         let params = Params::new(MEMORY_COST, TIME_COST, PARALLELISM, Some(OUTPUT_LEN)).unwrap();
         let argon2 = Argon2::new(Algorithm::Argon2id, Version::V0x13, params);
         let salt = SaltString::generate(&mut OsRng);
-        let result = argon2.hash_password(password.await.as_bytes(), &salt);
+        let result = argon2.hash_password(password.as_bytes(), &salt);
         assert!(result.is_ok());
     }
 }
